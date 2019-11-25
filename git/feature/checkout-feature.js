fc12018a1e7cfc61f60dca7f26d7c4c660b44a19
Renaud Lenoir <renaud.lenoir@gmail.com>
2019-11-25 14:40:57 +0100

    Added merge-feature function

--- git/feature/checkout-feature.js
+++ git/feature/checkout-feature.js
@@ -1,76 +1,48 @@
 const { exec } = require('child_process');
-const { existsSync, readdirSync } = require('fs');
+const { readdirSync } = require('fs');
 const { prompt } = require('inquirer');
 const vln = require('../../node/functions');
-const chalk = require('chalk');
 
 /**
  * Create a new feature branch for the selected project.
  */
+vln.promptProjects().then(answer => {
+    const projectName = answer.project;
 
-prompt([
-    {
-        type: 'list',
-        name: 'project',
-        message: 'Select project',
-        choices: readdirSync('../', { withFileTypes: true })
-            .filter(dirent => dirent.isDirectory())
-            .map(dirent => dirent.name)
-    },
-    {
-        type: 'input',
-        name: 'feature',
-        message: 'Name your feature'
-    },
-])
-    .then(answers => {
+    prompt([
+        {
+            type: 'input',
+            name: 'feature',
+            message: 'Name your feature'
+        },
+    ])
+        .then(answer => {
+            const featureName = answer.feature;
 
-        exec(`cd ../${answers.project}`, (err, stdout, stderr) => {
+            exec(`cd ../${projectName}`, (err, stdout, stderr) => {
 
-            prompt([{
-                type: 'confirm',
-                name: 'checkout',
-                message: `Create branch 'feature/${answers.feature}' for '${answers.project}' project`
-            }])
-                .then(answer => {
+                prompt([{
+                    type: 'confirm',
+                    name: 'checkout',
+                    message: `Create branch 'feature/${featureName}' for '${projectName}' project`
+                }])
+                    .then(answer => {
 
-                    if (answer.checkout) {
+                        if (answer.checkout) {
 
-                        exec(`git checkout -b feature/${answers.feature} develop`, (err, stdout, stderr) => {
-            
-                            if (stderr) {
-                                vln.err(stderr);
-                                return;
-                            }
+                            exec(`git checkout -b feature/${featureName} develop`, (err, stdout, stderr) => {
+                
+                                if (err) {
+                                    vln.err(stderr);
+                                    return;
+                                }
 
-                            vln.log(`Branch 'feature/${answers.feature}' created.`);
-                        });
-                    } else {
-                        vln.log(`Feature 'feature/${answers.feature}' checkout canceled.`);
-                    }
-                });
-            // exec('git rev-parse --abbrev-ref HEAD', (err, stdout, stderr) => {
-            //     const activeBranch = stdout;
-
-            //     exec('git checkout develop', (err, stdout, stderr) => {
-
-            //         if (stderr) {
-            //             console.log(chalk.blue('[VLN] ') + chalk.red('ERR! ') + 'Tasks | No \'develop\' branch.');
-            //             return;
-            //         }
-
-            //         exec('git status --porcelain', (err, stdout, stderr) => {
-    
-            //             if (stdout) {
-            //                 console.log(chalk.blue('[VLN] ') + chalk.red('ERR! ') + 'Tasks | \'develop\' branch unclean.');
-            //                 exec(`git checkout ${activeBranch}`);
-            //                 return;
-            //             }
-
-            //             console.log(chalk.blue('[VLN] ') + chalk.black.bgWhite('INFO ') + 'Tasks | \'develop\' branch unclean.');
-
-            //         });
-            //     });
-            // });
+                                vln.log(stdout);
+                            });
+                        } else {
+                            vln.log(`Checkout for branch 'feature/${featureName}' canceled.`);
+                        }
+                    });
+            });
         });
-    });
+});
